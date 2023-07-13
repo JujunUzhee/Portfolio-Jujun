5b67fcf021e9b71cc892cdba690709ab478687b7
JujunUzhee <103125165+JujunUzhee@users.noreply.github.com>
2023-07-13 11:38:21 +0700

    portfolio jujun

---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeindown: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        fadeinleft: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        fadeinright: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        'fade-in-top': {
+          '0%': { opacity: '0', transform: 'translateY(-20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-out ',
+        fadeindown: 'fadeindown 1s ease-out',
+        fadeinleft: 'fadeinleft 1s ease-out',
+        fadeinright: 'fadeinright 1s ease-out',
+        'fade-in-top': 'fade-in-top 0.5s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
